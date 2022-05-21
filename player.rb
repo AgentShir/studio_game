d01e695e81ed9a3536aa71c9e0da7a4df39bc6da
AgentShir <garciasal@gmail.com>
2022-05-20 20:23:23 -0700

    Updated files

--- player.rb
+++ player.rb
@@ -1,6 +1,8 @@
 require_relative 'treasure_trove'
+require_relative 'playable'
 
 class Player
+  include Playable
 
   attr_accessor :name
   attr_reader :health
@@ -10,16 +12,6 @@ class Player
     @health = health
     @found_treasures = Hash.new(0)
   end
-
-  def blam
-    @health -= 10
-    puts "#{@name} got blammed!"
-  end
-  
-  def w00t
-    @health += 15
-    puts "#{@name} got w00ted!"
-  end
   
   def score
     @health + points
@@ -27,10 +19,6 @@ class Player
   
   def points
     @found_treasures.values.reduce(0, :+)
-  end
-  
-  def strong?
-    @health > 100
   end
   
   def found_treasure(treasure)
